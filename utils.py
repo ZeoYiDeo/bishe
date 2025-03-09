ba7ca8b56004a9c64da64ebac8986a3d480e372b
ZeoYiDeo <121034549+ZeoYiDeo@users.noreply.github.com>
2025-03-09 19:38:59 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,16 +13,6 @@ import pandas as pd
 from tqdm import tqdm
 
 
-# Checkpoints
-def save_checkpoint(model, model_dir):
-    torch.save(model.state_dict(), model_dir)
-
-
-def resume_checkpoint(model, model_dir, device_id):
-    state_dict = torch.load(model_dir,
-                            map_location=lambda storage, loc: storage.cuda(device=device_id))  # ensure all storage are on gpu
-    model.load_state_dict(state_dict)
-
 
 # Hyper params
 def use_cuda(enabled, device_id=0):
